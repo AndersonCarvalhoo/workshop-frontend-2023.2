ec0c0dbf1b7ea981e6a7e7bf8f3732775416b799
Anderson Carvalho <andersoncarvalhoo2005@gmail.com>
2023-09-03 11:55:39 -0300

    second commit

--- galeria/script.js
+++ galeria/script.js
@@ -1,24 +1,28 @@
 let caracteristicas = document.getElementById('caracteristicas')
-let select = document.getElementById('nomes_personagens')
+let texto = document.getElementById('nomes_personagens')
+let select = document.getElementById('selecionar_personagens')
 let card = document.getElementById('card')
 let load = document.getElementById('load')
 let data;
-getPersonagens()
-async function getPersonagens() {
-    const Response = await fetch('https://swapi.dev/api/people/')
-    data = await Response.json()
-    load.style.display='none'; // Display none para o loading desaparecer depois de consumir o json
-    card.style.display='flex'; //Display flex para o card aparecer assim que o await do response.json() for concluido (valor padrão ta display: none;)
-
+function getPersonagemInput() {
+    dadosPersonagem(texto.value)
+    load.style.display='block'
+    caracteristicas.innerHTML = '' // usado para quando mudar a opção de personagem sumir as caracteristicas do antigo
 }
-function getPersonagem() {
-    opcao = select.value
-    caracteristicas.innerHTML = `<h3 style="margin: 10px 0px 5px; padding: 15px 30px; text-align:center;">${data.results[opcao].name}</h3> `
-    caracteristicas.innerHTML += `<p>Massa: ${data.results[opcao].mass}</p> `
-    caracteristicas.innerHTML += `<p>Cor de cabelo: ${data.results[opcao].hair_color}</p> `
-    caracteristicas.innerHTML += `<p>Cor de pele: ${data.results[opcao].skin_color}</p> `
-    caracteristicas.innerHTML += `<p>Cor dos olhos: ${data.results[opcao].eye_color}</p> `
-    caracteristicas.innerHTML += `<p>Data de aniversario: ${data.results[opcao].birth_year}</p> `
-    caracteristicas.innerHTML += `<p style="margin-bottom: 20px;">Sexo: ${data.results[opcao].gender}</p> `
+function getPersonagemSelected() {
+    dadosPersonagem(select.value)
+    load.style.display='block'
+    caracteristicas.innerHTML = ''
 }
-
+async function dadosPersonagem(opcao){
+    const response = await fetch('https://swapi.dev/api/people/?search='+opcao)
+    data = await response.json()
+    load.style.display='none'
+    caracteristicas.innerHTML = `<h3 style="margin: 10px 0px 5px; padding: 15px 30px; text-align:center;">${data.results[0].name}</h3> `
+    caracteristicas.innerHTML += `<p>Massa: ${data.results[0].mass}</p> `
+    caracteristicas.innerHTML += `<p>Cor de cabelo: ${data.results[0].hair_color}</p> `
+    caracteristicas.innerHTML += `<p>Cor de pele: ${data.results[0].skin_color}</p> `
+    caracteristicas.innerHTML += `<p>Cor dos olhos: ${data.results[0].eye_color}</p> `
+    caracteristicas.innerHTML += `<p>Data de aniversario: ${data.results[0].birth_year}</p> `
+    caracteristicas.innerHTML += `<p style="margin-bottom: 20px;">Sexo: ${data.results[0].gender}</p> `
+}
